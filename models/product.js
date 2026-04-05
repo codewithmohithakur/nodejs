f4fc7ab8ed9738585bf58641d6047c9381397605
Mohit <mohit@Mohits-MacBook-Pro.local>
2026-04-05 16:59:48 +0530

    Handling products

--- models/product.js
+++ models/product.js
@@ -5,7 +5,7 @@ const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
   'products.json'
-); 
+);
 
 const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
@@ -18,11 +18,15 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
